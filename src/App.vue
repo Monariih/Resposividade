69869ac0e2918ba778891a878dc3e6767de0fc30
Lucas Monari <lucas.monari.lm@outlook.com>
2023-01-16 14:17:23 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header id="cabecalho">
+    <button class="item" id="btn_opt"><img id="icon_menu" src="https://cdn-icons-png.flaticon.com/512/1243/1243926.png?w=740&t=st=1673358784~exp=1673359384~hmac=fda79b37e4d733221221de79d9f947768508a5e6cebfa6353ac6bf8e6cb4216f"></button>
+
+    <nav class="item" id="barra_nav">
+      <RouterLink to="/">Inicio</RouterLink>
+      <RouterLink to="/about">Info</RouterLink>
+    </nav>
+
+    <div id="foto_corsol"><img  id="logo_corsol" src="https://corsolar.com.br/wp-content/uploads/2022/03/corsolar-logo.png"></div>
+
+    <div id="last-line"></div>
+  </header>
+
+
+  <RouterView />
+</template>
+
+<style scoped>
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+nav a {
+
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+nav a:first-of-type {
+  border: 0;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,91 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+
+    <div id="usina">
+      <img id="usina" src="https://casacor.abril.com.br/wp-content/uploads/sites/7/2021/07/piqsels.com-id-swtfq.jpg?quality=80&strip=info">
+    </div>
+
+    <table>
+    <td>
+      <div id="card_info_dia">
+
+        <div id="eneGerada">
+          Energia gerada hoje:<br>
+          <span id="info_num">
+            10,3 KWh
+          </span>
+        </div>
+
+        <div id="div_img_info">
+          <img id="img_eneGerada" src="https://cdn-icons-png.flaticon.com/512/4114/4114970.png">
+        </div>
+
+
+      </div>
+    </td>
+    <td>
+      <div id="card_info_dinheiro">
+
+        <div id="eneGerada">
+          Saldo creditado hoje:<br>
+          <span id="info_num">
+            R$: 16,70
+          </span>
+        </div>
+
+        <div id="div_img_info">
+          <img id="img_eneGerada" src="https://img.icons8.com/bubbles/512/money-bag.png">
+        </div>
+
+      </div>
+    </td>
+    <tr>
+      <td>
+        <div id="card_info_media">
+
+          <div id="eneGerada">
+            Consumo médio diario:<br>
+            <span id="info_num">
+              13,9 KWp
+            </span>
+          </div>
+
+          <div id="div_img_info">
+            <img id="img_eneGerada" src="https://cdn-icons-png.flaticon.com/512/6301/6301557.png">
+          </div>
+
+        </div>
+      </td>
+      <td>
+        <div id="card_info_sustentavel">
+
+          <div id="eneGerada">
+            Total arvores plantadas:<br>
+            <span id="info_num">
+              329 :)
+            </span>
+          </div>
+
+          <div id="div_img_info">
+            <img id="img_eneGerada" src="https://cdn-icons-png.flaticon.com/512/490/490091.png">
+          </div>
+        </div>
+      </td>
+    </tr>
+    </table>
+    <div id="onde_vai_texto">
+      <div id="texto_explicativo">
+        <p id="titulo">Porque energia solar?</p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consequatur earum eius minima nisi rem rerum
+        sapiente tempore unde velit. Cumque id ipsa maiores minus placeat possimus qui, totam unde. Lorem ipsum dolor
+        sit amet, consectetur adipisicing elit. Accusantium, aliquid animi assumenda atque autem commodi dicta,
+        doloribus eius error excepturi id labore non odio possimus quam reiciendis repudiandae sequi voluptates!
+      </div>
+    </div>
+
+  </main>
+</template>
